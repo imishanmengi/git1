39b36897625e56f771bc3bea9ef178e444eb549e
ishan <ishanmengi2000@gmail.com>
2022-11-24 12:08:26 -0800

    updated

---
+++ task3q3.js
@@ -0,0 +1,12 @@
+//var headerTitle = document.getElementById('header-title');
+//var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent='hello';
+//headerTitle.textContent="hello my name is ishan";
+//header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].style.fontWeight ='bold';
+items[1].style.backgroundColor='green';
